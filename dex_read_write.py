997625324d87188d53af046d993204fb765d87fc
Matthew Begbie <md.begbie@mail.com>
2023-11-21 11:27:45 -0500

    created read and write module

---
+++ dex_read_write.py
@@ -0,0 +1,33 @@
+import json
+
+
+def write_authorization(data):
+    with open("authorization.json", "w") as outfile:
+        json.dump(data, outfile)
+
+
+def read_authorization():
+    try:
+        with open("./authorization.json", "r") as infile:
+            data = infile.read()
+            authorization = json.loads(data)
+            return (authorization)
+    except IOError:
+        print("IOERROR, could not find authorization.json")
+        return ("no file")
+
+
+def write_client_info(data):
+    with open("client.json", "w") as outfile:
+        json.dump(data, outfile)
+
+
+def read_client_info():
+    try:
+        with open("./client.json", "r") as infile:
+            data = infile.read()
+            client_info = json.loads(data)
+            return (client_info)
+    except IOError:
+        print("IOERROR, could not find client.json")
+        return ("no file")
